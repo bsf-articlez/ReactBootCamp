4f90d50eca905f4b32e067a516ef6e9a2e5893db
Sumet.F <bsf_articlez@outlook.com>
2019-02-02 11:20:12 +0700

    [week3] Init PropTypes for manage props

--- week3/src/components/MyInput/index.js
+++ week3/src/components/MyInput/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { MyInputStyled, MyInputWrapper, ClearButton } from "./MyInputStyled";
+import PropTypes from "prop-types";
 
 class MyInput extends Component {
   render() {
@@ -12,5 +13,19 @@ class MyInput extends Component {
     );
   }
 }
+
+// For default props
+MyInput.defaultProps = {
+  value: "",
+  onClear: () => {},
+  onChange: () => {}
+};
+
+// For validation props
+MyInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onClear: PropTypes.func,
+  onChange: PropTypes.func
+};
 
 export default MyInput;
