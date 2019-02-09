67b0b349cbb52d21569902634643d306b0d478a4
Sumet.F <bsf_articlez@outlook.com>
2019-02-09 10:55:01 +0700

    [week4] Add counterHook component

--- week4/src/components/CouterHooks/index.js
+++ week4/src/components/CouterHooks/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const CouterHook = () => {
-  return <div />;
+// useEffect จัดการเรื่อง life
+
+const initial = { count: 0 };
+
+const increseCount = setter => {
+  setter(({ count }) => ({ count: count + 1 }));
 };
+
+function CouterHook() {
+  const [count, setCount] = useState(initial);
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={increseCount(setCount)}>Click me please !!!!</button>
+    </div>
+  );
+}
 
 export default CouterHook;
