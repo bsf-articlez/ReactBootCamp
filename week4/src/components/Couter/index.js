95258da1478b294dc8536fb055a8bba3ef3dc296
Sumet.F <bsf_articlez@outlook.com>
2019-02-09 10:46:50 +0700

    [week4] Add couter compenent

---
+++ week4/src/components/Couter/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Couter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  onCount = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  };
+
+  render() {
+    const { count } = this.state;
+    return (
+      <div>
+        <span>{count}</span>
+        <button onClick={this.onCount}>Click me please !!!!</button>
+      </div>
+    );
+  }
+}
+
+export default Couter;
---
+++ week4/src/components/CouterHooks/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CouterHook = () => {
+  return <div />;
+};
+
+export default CouterHook;
