1c4e9ee76a872d20f064f21dd3e0e7a4f342dfbd
Sumet.F <bsf_articlez@outlook.com>
2019-02-16 11:03:02 +0700

    [week5] webpack config to dev

--- react-webpack-week5/webpack/config.dev.js
+++ react-webpack-week5/webpack/config.dev.js
@@ -1,4 +1,37 @@
-// const webpack = req
-module.exports = () => {
-  return {};
+const webpack = require("webpack");
+const part = require("path");
+
+module.exports = (env, args) => {
+  return {
+    mode: "development",
+    entry: "../index.js",
+    output: {
+      filename: "app.js",
+      path: part.resolve(__dirname, "../build"),
+      publicPath: "../public/"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: ["babel-loader"],
+          options: {
+            preset: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: ["last 2 versions"]
+                  },
+                  modules: false
+                }
+              ],
+              "@babel/preset-raect"
+            ]
+          }
+        }
+      ]
+    }
+  };
 };
