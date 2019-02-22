83f0af4d5deae9e86c0fde9cd0e57ed46e4aa51b
Sumet.F <bsf_articlez@outlook.com>
2019-02-22 14:29:57 +0700

    Updates

---
+++ precache-manifest.969545bdbc86741079699636d359f0e5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa479256f9efb0983885",
+    "url": "/bsf-articlez/ReactBootCamp/static/js/runtime~main.fa479256.js"
+  },
+  {
+    "revision": "d15b884a129362dc6e82",
+    "url": "/bsf-articlez/ReactBootCamp/static/js/main.d15b884a.chunk.js"
+  },
+  {
+    "revision": "4334b508b89ede5cf75d",
+    "url": "/bsf-articlez/ReactBootCamp/static/js/1.4334b508.chunk.js"
+  },
+  {
+    "revision": "903a0d58183f69d3bb3515461a32e41f",
+    "url": "/bsf-articlez/ReactBootCamp/index.html"
+  }
+];
--- week3/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Route from "./routes";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<Route />, rootElement);
