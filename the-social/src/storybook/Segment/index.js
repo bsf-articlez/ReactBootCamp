2dc38afa1586b971c6c9c9dfb748d190b4d754e7
Sumet.F <bsf_articlez@outlook.com>
2019-02-23 15:05:30 +0700

    [the-social] modify style

--- the-social/src/storybook/Segment/index.js
+++ the-social/src/storybook/Segment/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import Segment from 'components/Segment';
+import Segment from "components/Segment";
 
-storiesOf('Segment', module).add('with title', () => (
+storiesOf("Segment", module).add("with title", () => (
   <Segment title="My Segment" />
-))
+));
