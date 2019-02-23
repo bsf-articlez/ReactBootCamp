577d98774942926c230cd27d6ac08f40d262b3e7
Sumet.F <bsf_articlez@outlook.com>
2019-02-23 15:50:12 +0700

    [the-social] init image to profile-image

--- the-social/src/components/ProfileImage/index.js
+++ the-social/src/components/ProfileImage/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
 
-function ProfileImage({ src, icon, ...rest }) {
-    return <div className="imageWarpper">
-        
+function ProfileImage({ src, icon, size = "medium", ...rest }) {
+  if (!/^(small|medium|large)$/.test(size)) {
+    size = "medium";
+  }
+  const [imageLoaded, setImageLoaded] = useState(null);
+  useEffect(() => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => {};
+    image.onerror = () => {};
+  });
+  return (
+    <div className={`imageWarpper ${size}`}>
+      {imageLoaded ? <image src={imageLoaded} /> : <div>Loading...</div>}
     </div>
+  );
 }
 
 export default ProfileImage;
